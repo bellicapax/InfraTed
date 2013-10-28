46f3b9afdf3f62d22f2639bbfcccb9f8fb87cf8a
coalczar <josh.koleszar@gmail.com>
2013-10-28 09:52:03 -0500

    More Particle Work

---
+++ Assets/Scripts/Player/ParticleHit.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleHit : MonoBehaviour 
+{
+    public float freezeDecrement = 10.0f;
+	
+	private ParticleSystem.CollisionEvent[] collisionEvents = new ParticleSystem.CollisionEvent[16];
+	private CharacterInput scriptCharInput;
+	
+	void Start()
+	{
+		scriptCharInput = this.transform.parent.GetComponent<CharacterInput>();
+	}
+	
+	void OnParticleCollision(GameObject other)
+	{
+		ParticleSystem prt;
+		prt = other.GetComponent<ParticleSystem>();
+		int safeLength = prt.safeCollisionEventSize;
+        if (collisionEvents.Length < safeLength)
+            collisionEvents = new ParticleSystem.CollisionEvent[safeLength];
+		
+		int numCollisionEvents = prt.GetCollisionEvents(gameObject, collisionEvents);
+		for(int i = 0; i < numCollisionEvents; i++)
+		{
+			scriptCharInput.xTransferEnergy -= freezeDecrement * Time.deltaTime;
+            print("BOOM");
+		}
+	}
+}
